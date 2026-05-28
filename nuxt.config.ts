a850aececa7795018748883af29df1c7c1df0fc7
suriagadiego <suriaga.diego@gmail.com>
2026-05-28 17:25:49 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro: { preset: 'cloudflare-pages' },
+  nitro: { preset: 'cloudflare-module' },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   components: {
